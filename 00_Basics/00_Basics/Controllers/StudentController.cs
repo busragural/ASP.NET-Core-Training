c227841da04d3f700b43c5bab8b97b3b06618510
Busra Medine GURAL <busra.gural@monsternotebook.com>
2023-08-09 14:18:05 +0300

    get n delete

---
+++ 00_Basics/00_Basics/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using _00_Basics.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Xml.Linq;
+
+namespace _00_Basics.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All", Name = "GetStudents")]
+        public IEnumerable<Student> GetStudents()
+        {
+            return AllStudents.Students;
+
+        }
+        [HttpGet]
+        [Route("{id:int}", Name = "GetStudentbyId")]
+        public Student GetStudentbyId(int id)
+        {
+            return AllStudents.Students.FirstOrDefault(i => i.Id == id);
+            //return AllStudents.Students.Where(i => i.Id == id).FirstOrDefault();
+        }
+        [HttpGet("{name:alpha}", Name = "GetStudentbyName")]
+        public Student GetStudentbyName(string name)
+        {
+            return AllStudents.Students.FirstOrDefault(i => i.Name == name);
+            //return AllStudents.Students.Where(i => i.Id == id).FirstOrDefault();
+        }
+        [HttpDelete("{id}", Name="DeleteStudent")]
+        public bool DeleteStudent(int id)
+        {
+            var deleted = AllStudents.Students.FirstOrDefault(i => i.Id == id);
+            AllStudents.Students.Remove(deleted);
+            return true;
+        }
+
+    }
+}
---
+++ 00_Basics/00_Basics/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace _00_Basics.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
