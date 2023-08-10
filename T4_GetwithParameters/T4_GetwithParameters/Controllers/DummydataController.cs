3bd2e6dc5db2acd18a998ba5f41d53c6a21e1a67
Busra Medine GURAL <busra.gural@monsternotebook.com>
2023-08-10 09:36:35 +0300

    task4

---
+++ T4_GetwithParameters/T4_GetwithParameters/Controllers/DummydataController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using T3_Case_Get;
+
+namespace T4_GetwithParameters.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DummydataController : ControllerBase
+    {
+
+        private readonly ILogger<DummydataController> _logger;
+
+        public DummydataController(ILogger<DummydataController> logger)
+        {
+            _logger = logger;
+        }
+        private List<DummyData> GenerateDummyData(int count)
+        {
+            if(count > 0)
+            {
+                List<DummyData> dummyData = new List<DummyData>();
+
+                for(int i=1; i<=count; i++)
+                {
+                    DummyData model = new DummyData();
+                    model.Name = "Person" + i.ToString();
+                    model.Id = i;
+                    model.Created_date = DateOnly.FromDateTime(DateTime.Now.AddDays(i));    
+                    dummyData.Add(model);
+                }
+
+                return dummyData;
+            }
+            return null;
+        }
+
+        [HttpGet("{count}")]
+  
+        public IActionResult Get(int count)
+        {
+            List<DummyData> dList = GenerateDummyData(count);
+            if (dList == null)
+            {
+                return BadRequest("Count must be greater than 0");
+            }
+
+            return Ok(dList);
+        }
+
+        [HttpGet("{count}/{index}")]
+        public IActionResult Get(int count, int index)
+        {
+            List<DummyData> dList = GenerateDummyData(count);
+            if (index >= 0 && index < dList.Count)
+            {
+                DummyData element = dList[index-1];
+                DataList dataList = new DataList
+                {
+                    Data = dList,
+                    Element = element
+                };
+
+                return Ok(dataList);
+            }
+            
+             return BadRequest();
+       
+        }
+
+
+    }
+}
---
+++ T4_GetwithParameters/T4_GetwithParameters/Models/DummyData.cs
@@ -0,0 +1,18 @@
+﻿namespace T3_Case_Get
+{
+    public class DummyData
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public DateOnly Created_date { get; set; }
+
+    }
+
+
+    public class DataList
+    {
+        public List<DummyData> Data { get; set;}
+        public DummyData Element { get; set; }
+    }
+}
